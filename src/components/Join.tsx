c821ce0760041872dc6b1724e4ca153d4c6f9a96
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-12-23 14:32:18 -0300

    desktop style join finished

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -1,15 +1,19 @@
+import '../styles/components/join.css';
+
 import imageJoin from '../images/join.svg';
 
 export default function Join() {
   return (
     <section className='join' id='join'>
-      <div className='join__image'>
-        <img src={imageJoin} alt="Join"/>
-      </div>
       <div className='join__email'>
         <label htmlFor='email'>Join our membership to get special offer</label>
-        <input id='email' type="email" placeholder='Enter your email address'/>
-        <button>Join</button>
+        <div className="email__input">
+          <input id='email' type="email" placeholder='Enter your email address'/>
+          <button>Join</button>
+        </div>
+      </div>
+      <div className='join__image'>
+        <img src={imageJoin} alt="Join"/>
       </div>
     </section>
   )
