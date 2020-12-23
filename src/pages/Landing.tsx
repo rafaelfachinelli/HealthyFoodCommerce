eeb4d7898844faac9f47124b3b7bb7b178859dac
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-12-23 12:08:11 -0300

    join added in landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import Main from '../components/Main';
 import Recipes from '../components/Recipes';
 import Services from '../components/Services';
 import Blog from '../components/Blog';
+import Join from '../components/Join';
 
 import '../styles/pages/landing.css';
 
@@ -12,6 +13,7 @@ export default function Landing() {
       <Recipes/>
       <Services/>
       <Blog/>
+      <Join/>
     </>
   );
 }
