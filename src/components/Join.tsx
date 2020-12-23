54786768b1d91444349ddf3ce0612a214621b7eb
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-12-23 13:06:02 -0300

    new eslint and prettier

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -3,7 +3,14 @@ import imageJoin from '../images/join.svg';
 export default function Join() {
   return (
     <section className='join' id='join'>
-
+      <div className='join__image'>
+        <img src={imageJoin} alt="Join"/>
+      </div>
+      <div className='join__email'>
+        <label htmlFor='email'>Join our membership to get special offer</label>
+        <input id='email' type="email" placeholder='Enter your email address'/>
+        <button>Join</button>
+      </div>
     </section>
   )
 }
